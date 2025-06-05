a36c88873ee60f452a02074e4832e8e21b7a4be2
Akash Yadav <85032480+yadavgkp@users.noreply.github.com>
2025-06-05 13:36:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
